716642a0c97d16d073b81d704ff82e6371341b91
gurvinder13389 <36071545+gurvinder13389@users.noreply.github.com>
2019-05-03 09:40:57 -0400

    Add files via upload
    Added code

---
+++ src/main/java/newModule/NewProject/App.java
@@ -0,0 +1,13 @@
+package newModule.NewProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/newModule/NewProject/TestCase.java
@@ -0,0 +1,76 @@
+package newModule.NewProject;
+
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestCase {
+	@Test
+	public void TC01() throws InterruptedException
+	{
+		System.setProperty("webdriver.gecko.driver", "resources/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://twitter.com/stepin_forum");
+		
+		List<WebElement> lstWe = driver.findElements(By.xpath("//div[@title='Retweet']/following-sibling::span/span[@class='ProfileTweet-actionCountForPresentation']"));
+		int highest = 0;
+		for(WebElement we : lstWe)
+		{
+			int temp = Integer.parseInt(we.getText());
+			
+			if(temp>highest)
+			{
+				highest = temp;
+			}
+		}
+		
+		System.out.println(highest);
+		
+		
+		lstWe = driver.findElements(By.xpath("//div[@title='Like']/following-sibling::span/span[@class='ProfileTweet-actionCountForPresentation']"));
+		int highestLike = 0;
+		for(WebElement we : lstWe)
+		{
+			if(!we.getText().equals(""))
+			{
+				int temp = Integer.parseInt(we.getText());
+				
+				if(temp>highestLike)
+				{
+					highestLike = temp;
+				}
+			}
+		}
+		
+		System.out.println(highestLike);
+		//div[@title='Like']/following-sibling::span/span[@class='ProfileTweet-actionCountForPresentation']
+		
+//		boolean bln = true;
+//		
+//		while(bln)
+//		{
+//			int val = driver.findElements(By.xpath("//div[@title='Retweet']/following-sibling::span/span[@class=\"ProfileTweet-actionCountForPresentation\"]")).size();
+//			
+//			if(val>=50)
+//			{
+//				bln=false;
+//			}
+//			else
+//			{
+//				Thread.sleep(2000);
+//				JavascriptExecutor js = (JavascriptExecutor)driver;
+//				js.executeScript("window.scrollTo(0,document.body.scrollheight)");
+//				System.out.println(driver.findElements(By.xpath("//div[@title='Retweet']/following-sibling::span/span[@class=\"ProfileTweet-actionCountForPresentation\"]")).size());
+//			}
+//		}
+	}
+}
